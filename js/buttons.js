6c0936e9a69b88e2d2a5449427fc22b1b9afc1cd
Viktor <castesto@yandex.ru>
2026-05-27 03:14:41 +0300

    feat: add maps links

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,26 @@
 document.querySelector('.historyButton').addEventListener('click', function () {
     window.location.href = 'about-cat.html';
 });
+
+const moreSpecialistsBtn = document.querySelector('.moreSpecialistsButton');
+if (moreSpecialistsBtn) {
+    moreSpecialistsBtn.addEventListener('click', () => {
+        window.location.href = 'medics.html';
+    });
+}
+
+const yandexMapBtn = document.querySelector('.yandexMap');
+if (yandexMapBtn) {
+    yandexMapBtn.addEventListener('click', () => {
+        const address = encodeURIComponent('Краснодар, ул. Пушкина 14');
+        window.open(`https://yandex.ru/maps/?text=${address}`, '_blank');
+    });
+}
+
+const gisMapBtn = document.querySelector('.gisMap');
+if (gisMapBtn) {
+    gisMapBtn.addEventListener('click', () => {
+        const address = encodeURIComponent('Краснодар ул. Пушкина 14');
+        window.open(`https://2gis.ru/krasnodar/search/${address}`, '_blank');
+    });
+}
